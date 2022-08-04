b383be24512903e60afc34f4380b8cda99cb0db1
unknown <u.erkinov@inbox.ru>
2022-08-04 16:49:00 +0500

    feat: half done

--- category/views.py
+++ category/views.py
@@ -6,14 +6,22 @@ from django.db import models
 
 from common.models import User
 from category import serializer
-
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.vary import vary_on_headers
 # Create your views here.
 
 
 class VacancyListView(generics.ListAPIView):
-    queryset = Vacancy.objects.all()
+    queryset = Vacancy.objects.filter('company','vacancy', 'employer').count()
     serializer_class = serializer.VacancySerializer
 
-    def get_queryset(self):
-        return
+    @method_decorator(cache_page(60*60*2))
+    @method_decorator(vary_on_headers("Authorization",))
+    def get(self, *args, **kwargs):
+        return super().get(*args, **kwargs)
+
+    
 
